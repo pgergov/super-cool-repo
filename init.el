57c0b1f99761245d41e235b56df9e2ceec549211
Pavlin Gergov <gergov.pavlin@gmail.com>
2021-02-17 23:39:09 +0200

    Add latest macOS setup

--- init.el
+++ init.el
@@ -1,142 +1,138 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;  General configs  ;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  General configuration  ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
-;; keep installed packages in .emacs.d
+;; Keep installed packages in .emacs.d
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
 (package-initialize)
 
-;; store all backup and autosave files in the tmp dir
+;; Store all backup and autosave files in the tmp dir
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; update package metadata if the local cache is missing
+;; Update package metadata if the local cache is missing
 (unless package-archive-contents
   (package-refresh-contents))
 
+;; Default credentials
 (setq user-full-name "Pavlin Gergov"
       user-mail-address "gergov.pavlin@gmail.com")
 
 ;; Always load newest byte code
 (setq load-prefer-newer t)
 
-;; reduce the frequency of garbage collection by making it happen on
+;; Reduce the frequency of garbage collection by making it happen on
 ;; each 50MB of allocated data (the default is on every 0.76MB)
 (setq gc-cons-threshold 50000000)
 
-;; osx specific
+;; Osx specific
 (when (eq system-type 'darwin)
-  ;; use cmd as meta key
+  ;; Use cmd as meta key
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier nil))
-  ;; disable the annoying bell ring
+  ;; Disable the annoying bell ring
   (setq ring-bell-function 'ignore)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  GUI configuration  ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; no need for useles pop up window
+;; No need for useles pop up window
 (setq ns-pop-up-frames nil)
 
-;; or welcome screen
+;; Or welcome screen
 (setq inhibit-startup-screen t)
 
-;; or toolbar
+;; No need for toolbar
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 
-;; blinkless cursor
+;; Blinkless cursor
 (blink-cursor-mode -1)
 
-;; highlight the current line
+;; Highlight the current line
 (global-hl-line-mode +1)
 
-;; nice scrolling
+;; Nice scrolling
 (setq scroll-margin 0
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
 
-;; line settings
+;; Default font-size
+(set-face-attribute 'default nil :height 160)
+
+;; Line settings
 (line-number-mode t)
 (column-number-mode t)
 (size-indication-mode t)
 
-;; default font-size
-(set-face-attribute 'default nil :height 140)
-
 ;; Newline at end of file
 (setq require-final-newline t)
 
-;; more useful frame title, that show either a file or a
+;; More useful frame title, that show either a file or a
 ;; buffer name (if the buffer isn't visiting a file)
 (setq frame-title-format
       '((:eval (if (buffer-file-name)
                    (abbreviate-file-name (buffer-file-name))
                  "%b"))))
 
-;; automatically refresh when file changes on disk
+;; Automatically refresh when file changes on disk
 (global-auto-revert-mode t)
 
-;; enable y/n answers
+;; Enable y/n answers
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; split vertically instead of horizontal
+;; Split vertically instead of horizontal
 (setq split-height-threshold nil)
 (setq split-width-threshold 120)
 
-;; smart tab behavior - indent or complete
+;; Smart tab behavior - indent or complete
 (setq tab-always-indent 'complete)
-
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 
 ;;;;;;;;;;;;;;;;;;;;
-;;;;  packages  ;;;;
+;;;;  Packages  ;;;;
 ;;;;;;;;;;;;;;;;;;;;
 
-;; handle package configuration with `use-package`
+;; Handle package configuration with `use-package`
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
+
 (require 'use-package)
-;; indicate loading activity
+
+;; Indicate loading activity
 (setq use-package-verbose t)
 
+;; Blackish theme
 (use-package dracula-theme
   :ensure t)
 
-(use-package anaconda-mode
-  :ensure t)
-
-(use-package elpy
+;; Better completion
+(use-package ivy
   :ensure t
   :config
-  (elpy-enable))
+    (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
 
-(use-package git-gutter
-  :ensure t
-  :config
-  (global-git-gutter-mode +1)
-  (set-face-foreground 'git-gutter:modified "yellow"))
-
-(use-package neotree
-  :ensure t
-  :config
-  (setq neo-theme 'ascii))
-
+;; The one and only silver search
 (use-package ag
   :ensure t)
 
+;; Navigate projects
 (use-package projectile
   :ensure t
+  :init
+  (setq projectile-keymap-prefix (kbd "C-c p"))
   :bind
   ("C-c k" . projectile-find-file)
   ("C-c j" . projectile-switch-to-buffer)
@@ -144,51 +140,42 @@
   (setq projectile-completion-system 'ivy)
   (projectile-global-mode +1))
 
-(use-package rainbow-delimiters
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
+;; Side project tree
+(use-package neotree
   :ensure t
   :config
-  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+  (setq neo-theme 'ascii))
 
-(use-package dumb-jump
+;; Pretty git changes indicators
+(use-package git-gutter
   :ensure t
   :config
-  (dumb-jump-mode))
+  (global-git-gutter-mode +1)
+  (set-face-foreground 'git-gutter:modified "yellow"))
 
-(use-package prettier-js
+;; The Python must package
+(use-package elpy
   :ensure t
   :config
-  (setq prettier-js-args '(
-    "--print-width" "80"
-    "--tab-width" "2"
-    "--single-quote" "true"
-    "--trailing-comma" "none"
-    "--no-bracket-spacing" "false"
-    "--jsx-bracket-same-line" "true"
-  )))
+  (elpy-enable)
+  (setq elpy-rpc-virtualenv-path 'current)
+  (add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1))))
 
-(use-package js2-mode
-  :ensure t
-  :config
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-  (setq-default js2-basic-offset 2))
-
-(use-package rjsx-mode
+;; Python docs, lookups and completion
+(use-package anaconda-mode
   :ensure t)
 
-(use-package js2-refactor
+;; Automatically load the .python-version env when switching between projects
+(use-package auto-virtualenv
   :ensure t
   :config
-  (add-hook 'js2-mode-hook #'js2-refactor-mode)
-  (js2r-add-keybindings-with-prefix "C-c C-r")
-  (define-key js2-mode-map (kbd "C-k") #'js2r-kill))
+  (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv))
 
-(use-package xref-js2
-  :ensure t
-  :config
-  (define-key js2-mode-map (kbd "M-.") nil)
-  (add-hook 'js2-mode-hook (lambda ()
-    (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
-
+;; Typescript setup (-_^)
 (defun setup-tide-mode ()
   (interactive)
   (tide-setup)
@@ -196,6 +183,9 @@
   (eldoc-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
   (company-mode +1))
 
 (use-package tide
@@ -213,36 +203,47 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-markup-indent-offset 2)
   (add-hook 'web-mode-hook
-	    (lambda ()
-	      (setup-tide-mode))))
+        (lambda ()
+          (setup-tide-mode))))
 
+;; Spot brackets easier
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+;; Drag regions
 (use-package drag-stuff
   :ensure t
   :config
   (drag-stuff-global-mode 1)
   (drag-stuff-define-keys))
 
-(use-package json-mode
-  :ensure t)
-
-(use-package ivy
+;; Edit yaml
+(use-package yaml-mode
   :ensure t
   :config
-    (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t))
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
 
-(use-package indent-tools
-  :ensure t)
+;; Pretty markdown files
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
  '(package-selected-packages
-   (quote
-    (jedi indent-tools web-mode tide json-mode drag-stuff xref-js2 js2-refactor rjsx-mode js2-mode prettier-js dumb-jump rainbow-delimiters projectile ag neotree git-gutter elpy anaconda-mode use-package dracula-theme))))
+   '(auto-virtualenv markdown-mode ivy drag-stuff web-mode yaml-mode tide rainbow-delimiters projectile use-package neotree git-gutter elpy dracula-theme anaconda-mode ag)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -251,7 +252,7 @@
  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;  custom functions  ;;;;
+;;;;  Custom functions  ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun my-python-breakpoint ()
@@ -278,16 +279,6 @@
     (previous-line)
     (indent-for-tab-command)))
 
-(defun my-goto-definition ()
-  "Elpy fails to find decorated python functions, dumb-jump-go doesn't."
-  (interactive)
-  (let ((initial_func (thing-at-point 'symbol)))
-    (elpy-goto-definition)
-    (when (not (equal initial_func (thing-at-point 'symbol)))
-      (progn
-	(dumb-jump-back)
-	(dumb-jump-go)))))
-
 (defun my-neotree-project-dir-toggle ()
   "Open NeoTree using project root via `find-file-in-project` or the current buffer directory."
   (interactive)
@@ -310,28 +301,29 @@
             (neotree-find file-name))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;  key bindings  ;;;;
+;;;;  Key bindings  ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Find files
 (global-set-key (kbd "C-;") 'find-file)
-(global-set-key (kbd "C-o") 'other-window)
-(global-set-key (kbd "M-,") 'pop-tag-mark)
+;; Find file in another window
 (global-set-key (kbd "C-M-;") 'find-file-other-window)
+;; Switch window
+(global-set-key (kbd "C-o") 'other-window)
+;; Previous opened window
 (global-set-key (kbd "<C-tab>") 'mode-line-other-buffer)
-(global-set-key (kbd "C-x p") 'prettier-js)
-
-;; use `d` to delete characters
-(global-set-key (kbd "C-M-d") 'delete-char)
+;; Return one step back
+(global-set-key (kbd "M-,") 'pop-tag-mark)
+;; Use `d` to delete characters
 (global-set-key (kbd "C-d") 'delete-backward-char)
-
-;; copy current line
+(global-set-key (kbd "C-M-d") 'delete-char)
+;; Copy current line
 (global-set-key "\C-c\c" "\C-a\C- \C-n\M-w")
-;; replace buffer-menu with ibuffer
+;; Replace buffer-menu with ibuffer
 (global-set-key (kbd "C-x C-b") #'ibuffer)
 
-;; custom funcs
+;; Custom functions bindings
 (global-set-key (kbd "C-c i") 'my-python-breakpoint)
 (global-set-key (kbd "C-c o") 'my-insert-line-before)
-(define-key elpy-mode-map (kbd "M-.") 'my-goto-definition)
 (global-set-key (kbd "C-c t") 'my-neotree-project-dir-toggle)
 (global-set-key (kbd "C-c m") 'my-rack-brackets-on-new-indented-line)
